0c315e246335027db15ceb51681565b2d58a60ac
Vadim <chakapega@gmail.com>
2019-05-07 01:04:25 +0300

    refactoring

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ const main = document.querySelector('main');
 const clearButton = document.querySelector('.clear-button');
 
 const getWeather = city => {
-
   return new Promise((resolve, reject) => {
 
     const URL = 'http://api.apixu.com/v1/current.json?key=502e6d2bb7fc49b7b55164834192904&q=';
@@ -12,7 +11,6 @@ const getWeather = city => {
 
     req.open('GET', `${URL}` + city);
     req.onload = () => {
-  
       let allDataWeather = JSON.parse(req.response);
       
       if(req.status === 200) {
@@ -20,29 +18,21 @@ const getWeather = city => {
       } else if(req.status === 400) {
         reject(allDataWeather);
       } else{
-        alert('Error!')
+        alert('Error!');
       };
-      
     };
-
     req.onerror = () => {
       alert('No connection to the server or the server is not responding');
     };
-
     req.send();
-
   });
-
 };
 
 const showResults = element => {
-
   main.appendChild(element);
-
 };
 
 const createElemForResults = allDataWeather => {
-
   const resultsContainer = document.createElement('div');
   const location = document.createElement('span');
   const condition = document.createElement('div');
@@ -88,47 +78,34 @@ const createElemForResults = allDataWeather => {
   resultsContainer.appendChild(temperature);
 
   return resultsContainer;
-
 };
 
 const deletionResultsContainer = () => {
-
   if(document.querySelector('.results__container')) {
     document.querySelector('.results__container').remove();
   };
-    
 };
 
 const getCityNames = () => {
-  
   return new Promise(resolve => {
-
     const urlCity = 'http://api.apixu.com/v1/search.json?key=502e6d2bb7fc49b7b55164834192904&q=';
     const req = new XMLHttpRequest();
 
     req.open('GET', `${urlCity}` + choiceCityFormInput.value);
     req.onload = () => {
-
       let citiesList = JSON.parse(req.response);
       resolve(citiesList);
-      
     };
-  
     req.send();
-
   });
-
 };
 
 const createProposedListOfCities = arr => {
-
   const proposedListOfCities = document.createElement('div');
 
   proposedListOfCities.classList.add('proposed-list-of-cities__container');
   proposedListOfCities.addEventListener('click', e => {
-
     choiceCityFormInput.value = e.target.textContent;
-
   });
 
   for(let i = 0; i < arr.length; i++) {
@@ -139,27 +116,21 @@ const createProposedListOfCities = arr => {
   };
 
   return proposedListOfCities;
-
 };
 
 const deletionProposedListOfCities = () => {
-
   const deleteCount = document.querySelectorAll('.proposed-list-of-cities__container').length;
 
   for(let i = 0; i < deleteCount; i++) {
     document.querySelector('.proposed-list-of-cities__container').remove();
   };
-    
 };
 
 const showProposedListOfCities = element => {
-
   main.appendChild(element);
-
 };
 
 choiceCityForm.addEventListener('submit', e => {
-  
   e.preventDefault();
 
   const city = e.target['choice-city-form__input'].value;
@@ -173,40 +144,30 @@ choiceCityForm.addEventListener('submit', e => {
       allDataWeather => {
         alert(allDataWeather.error.message);
       });
-
 });
 
 choiceCityFormInput.addEventListener('keyup' , () => {
-
   deletionProposedListOfCities();
 
   if(choiceCityFormInput.value.length > 2) {
     getCityNames()
       .then(
         citiesList => {
-
           if(citiesList.length > 0) {
             showProposedListOfCities(createProposedListOfCities(citiesList));
           };
-
         });
-
   };
-  
 });
 
 choiceCityFormInput.addEventListener('blur' , () => {
-  
   setTimeout(deletionProposedListOfCities, 200);
-  
 });
 
 clearButton.addEventListener('click', () => {
-
   deletionProposedListOfCities();
   deletionResultsContainer();
   choiceCityFormInput.value = "";
-
 });
 
 
